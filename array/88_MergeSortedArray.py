4665593d525de56bcfdcf77c3a4ab7aef6e15ffc
longwenxiu <long_wenxiu@163.com>
2018-06-20 22:27:49 +0800

    pop and [:q+1]

---
+++ array/88_MergeSortedArray.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+法一：
+用sort
+"""
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        tag=len(nums1)
+        while tag>m:
+            nums1.pop()
+            tag-=1
+        nums1.extend(nums2[0:n])
+        nums1.sort()
+
+"""
+法二：
+从后往前遍历
+"""
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        p, q, k = m-1, n-1, m+n-1
+        while p >= 0 and q >= 0:
+            if nums1[p] > nums2[q]:
+                nums1[k] = nums1[p]
+                p, k = p-1, k-1
+            else:
+                nums1[k] = nums2[q]
+                q, k = q-1, k-1
+        nums1[:q+1] = nums2[:q+1]
