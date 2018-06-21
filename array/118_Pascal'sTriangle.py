38ced364f00d10988953df0997155b0101707f14
longwenxiu <long_wenxiu@163.com>
2018-06-21 16:22:27 +0800

    Create 118_Pascal'sTriangle.py

---
+++ array/118_Pascal'sTriangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def generate(self, num_rows):
+        triangle = []
+
+        for row_num in range(num_rows):
+            # The first and last row elements are always 1.
+            row = [None for _ in range(row_num+1)]
+            row[0], row[-1] = 1, 1
+            """
+            10 11行可以变为triangle.append([1]*(i+1))来初始化array
+            """
+            # Each triangle element is equal to the sum of the elements
+            # above-and-to-the-left and above-and-to-the-right.
+            for j in range(1, len(row)-1):
+                row[j] = triangle[row_num-1][j-1] + triangle[row_num-1][j]
+
+            triangle.append(row)
+
+        return triangle
+        
+"""
+法二：
+最基础的解法
+"""
+class Solution:
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if numRows == 0:
+            return []
+        res = [[1]]
+        for i in range(1, numRows):
+            res.append([])
+            for j in range(i+1):
+                res[i].append((res[i-1][j-1] if j>0 else 0) + (res[i-1][j] if j<i else 0))
+        return res
